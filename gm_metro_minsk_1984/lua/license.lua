a5e2563217acbbbbde8fb268fb88fef3bb97fa5d
robilkot <timur.robilko@gmail.com>
2021-07-31 23:16:09 +0300

    #40

---
+++ gm_metro_minsk_1984/lua/license.lua
@@ -0,0 +1,78 @@
+MetroPack team:
+metropacker - Timur Robilko Markovich
+SawMal - Dmitrii Atrashkov Olegovich
+VitroMod - Виктор Андреевич Троицкий
+FeDoT - Fedor Bolomatov Nikolaevich
+ArtPalu - Pavel Ludilov Alekseevich 
+
+--------------------------------------------------------------------------------
+License for:
+  \lua\*.*       			All Lua files
+  \maps\*.*					Map files
+  \models\*.*      		   	Art assets for the map
+  \materials\*.*			Art assets for the map
+  \sounds\*.*				Sounds for the map
+	
+--------------------------------------------------------------------------------
+       Copyright (C) 2019-2021 MetroPack
+        Last updated: 31st July, 2021
+					
+These files contain proprietary code and assets that belong to MetroPack team
+and are provided solely for limited personal use with Garry's Mod.
+The following restrictions apply:
+
+1. These files can only be redistributed through
+   Steam Workshop for use with Garry's Mod.
+ 
+2. The software code contained in the files may only be executed under Garry's Mod
+   Lua interpreter.
+
+3. Any commercial use of these assets and code is strictly forbidden without prior
+   written agreement, including but not limited to use in industrial or commercial
+   training simulators, simulation videogames.
+  
+4. The use of these assets and code for any purposes of professional education is
+   strictly forbidden without prior written agreement, including but not limited
+   to use in non-commercial training simulators or simulators installed in
+   facilities related to professional education.
+  
+5. It is forbidden to create and distribute derived or modified versions of the software code
+   without an explicit written permission from MetroPack team.
+   
+   The art assets contained in the files belong to their corresponding authors
+   and may not be distributed, used or modified without written permission from
+   MetroPack team or the original copyright holders.
+
+6. The use of these assets and software code is forbidden for non-commercial public
+   purposes, including but not limited to use in museums and exhibitions.
+   
+   MetroPack team explicitly grants permission to record, stream, share
+   and monetize screenshots, audio, video footage derived from Garry's Mod.
+   
+7. Additional art assets belong to their corresponding authors and are distributed
+   under this license with their permission.
+
+          A custom license on these files may be obtained by
+          contacting authors at metropack.team@gmail.com
+
+--------------------------------------------------------------------------------
+License for: other assets and files not covered by license above	
+--------------------------------------------------------------------------------
+Copyright (C) MetroPack team 2019-2021
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
