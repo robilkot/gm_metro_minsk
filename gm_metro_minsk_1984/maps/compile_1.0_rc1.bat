a92cff8cf968a674d657390d609b992580239f1d
klusandr <klusandr@mail.ru>
2021-09-21 11:28:15 +0000

    Revert "add feeder"
    This reverts commit 8122897862edad7cc4e61e6ab1ee595dde0744b1

--- gm_metro_minsk_1984/maps/compile_1.0_rc1.bat
+++ gm_metro_minsk_1984/maps/compile_1.0_rc1.bat
@@ -1,8 +1,8 @@
-"E:\Steam Game\steamapps\common\GarrysMod\bin\vmfii.exe" "E:\Steam Game\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk.vmf" "E:\Steam Game\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vmfii.exe" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk.vmf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
 
-"E:\Steam Game\steamapps\common\GarrysMod\bin\vbsp.exe" -game "E:\Steam Game\steamapps\common\GarrysMod\garrysmod" -notjunc "E:\Steam Game\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vbsp.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" -notjunc "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
 
-"E:\Steam Game\steamapps\common\GarrysMod\bin\vvis.exe" -game "E:\Steam Game\steamapps\common\GarrysMod\garrysmod" "E:\Steam Game\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vvis.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
 
-"E:\Steam Game\steamapps\common\GarrysMod\bin\vrad.exe" -bounce 10 -fastambient -noextra -game "E:\Steam Game\steamapps\common\GarrysMod\garrysmod" "E:\Steam Game\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vrad.exe" -bounce 10 -fastambient -noextra -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\gm_metro_minsk_1984.vmf"
 pause
