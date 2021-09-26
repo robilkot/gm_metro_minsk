94c87c82ddef1d32d033be6ff64ad6fc529f4b8c
robilkot <timur.robilko@gmail.com>
2021-09-26 16:34:51 +0300

    general compile

--- gm_metro_minsk_1984/maps/compile_hdrtest.bat
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vbsp.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" -leaktest -low -notjunc "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\ya.vmf"
-
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vvis.exe" -fast -low "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\ya.bsp"
-
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vrad.exe" -both -fastambient -noextra -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod"  "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\ya.bsp"
-pause 
--- gm_metro_minsk_1984/maps/compile_institut.bat
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vbsp.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" -leaktest -low -notjunc "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\institut.vmf"
-pause
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vvis.exe" -fast -low "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\institut.bsp"
-pause
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\vrad.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" -fast "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\gm_metro_minsk_1984\maps\institut.bsp"
-pause 
