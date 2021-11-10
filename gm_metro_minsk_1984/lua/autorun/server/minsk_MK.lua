61f825f129fdc07d0fa1d7c37c2989bfaf23e49b
klusandr <65304648+klusandr@users.noreply.github.com>
2021-11-10 21:37:57 +0300

    add mk signal close

--- gm_metro_minsk_1984/lua/autorun/server/minsk_MK.lua
+++ gm_metro_minsk_1984/lua/autorun/server/minsk_MK.lua
@@ -24,38 +24,77 @@ local MKNameList = {
 	"MK17",
 } -- MK names
 
+local function getFile(path, name)
+    local data,found
+    if file.Exists(Format(path..".txt",name),"DATA") then
+        data= util.JSONToTable(file.Read(Format(path..".txt",name),"DATA"))
+        found = true
+    end
+    if not data and file.Exists(Format(path..".lua",name),"LUA") then
+        data = util.JSONToTable(file.Read(Format(path..".lua",name),"LUA"))
+        found = true
+    end
+    if not found then
+        --print(Format("%s definition file not found: %s",id,Format(path,name)))
+        return
+    elseif not data then
+        --print(Format("Parse error in %s %s definition JSON",id,Format(path,name)))
+        return
+    end
+    return data
+end
+
 
 ----   API definition   ----
 
 Minsk = Minsk or {}
 Minsk.MK = Minsk.MK or {}
 
-function Minsk.MK.Initialize()    
-    Minsk.MK.CloseButtonList = {}
-    Minsk.MK.OpenButtonList = {}
-    Minsk.MK.IndicatorList = {}
-    
-    local buttonList = ents.FindByClass("func_button")
-    local indicatorList = ents.FindByClass("prop_dynamic")
+function Minsk.MK.Initialize()
+    Minsk.MK.List = {}
 
-    for _, ent in ipairs(buttonList) do
-        for _, MKName in ipairs(MKNameList) do
-            
+    for _, MKName in pairs(MKNameList) do
+        Minsk.MK.List[MKName] = {}
+    end
+
+    for _, ent in ipairs(ents.FindByClass("func_button")) do
+        for _, MKName in ipairs(MKNameList) do        
             if (ent:GetName() == MKName.."close") then
-                Minsk.MK.CloseButtonList[MKName] = ent
+                Minsk.MK.List[MKName].CloseButton = ent
             end
             if (ent:GetName() == MKName.."open") then
-                Minsk.MK.OpenButtonList[MKName] = ent
+                Minsk.MK.List[MKName].OpenButton = ent
             end
         end
     end
     
-    for _, ent in ipairs(indicatorList) do
+    for _, ent in ipairs(ents.FindByClass("prop_dynamic")) do
         for _, MKName in ipairs(MKNameList) do
             if (ent:GetName() == MKName.."check") then
-                Minsk.MK.IndicatorList[MKName] = ent
+                Minsk.MK.List[MKName].Indicator = ent
             end
         end
+    end
+
+    for _, ent in ipairs(ents.FindByClass("func_door_rotating")) do
+        for _, MKName in ipairs(MKNameList) do
+            if (ent:GetName() == MKName.."col") then
+                Minsk.MK.List[MKName].Entity = ent
+            end
+        end
+    end
+
+    if (not Minsk.Server) then
+        timer.Simple(3.0, Minsk.MK.LoadSignals)
+
+        local m_load = Metrostroi.Load
+        function Metrostroi.Load(name,keep_signs)
+            m_load(name,keep_signs)
+            timer.Simple(1.0, Minsk.MK.LoadSignals)
+        end
+
+        hook.Add("Think", "Minsk.MK.Think", Minsk.MK.Think)
+
     end
 
     if (not game.SinglePlayer()) then
@@ -63,27 +102,120 @@ function Minsk.MK.Initialize()
     end
 end
 
+if (not Minsk.Server) then
+    
+    function Minsk.MK.LoadSignals(name)
+        local data = getFile("metrostroi_data/mk_signs_%s", name or game.GetMap())
+        
+        if (not data) then return end
+
+        for MKName, signalsName in pairs(data) do
+            if (Minsk.MK.List[MKName]) then
+                Minsk.MK.List[MKName].SignalList = {}
+                for _, signalName in pairs(signalsName) do
+                    local signal = Metrostroi.GetSignalByName(signalName)
+                    print(signal)
+                    if (signal) then
+                        table.insert(Minsk.MK.List[MKName].SignalList, signal)
+                    end
+                end
+            end
+        end
+    end
+
+    function Minsk.MK.Think()
+        for MKName, MK in pairs(Minsk.MK.List) do
+            if (MK.Entity:IsValid() and MK.SignalList) then
+                local mkRotation = MK.Entity:GetInternalVariable("m_angAbsRotation")[2]
+
+                if (mkRotation < 0) then
+                    if (not MK.IsClose) then
+                        Minsk.MK.CloseSignals(MKName)
+                    end
+
+                    MK.IsClose = true 
+                else
+                    if (MK.IsClose) then
+                        Minsk.MK.OpenSignals(MKName)
+                    end
+
+                    MK.IsClose = false 
+                end  
+            end
+        end
+    end
+
+    function Minsk.MK.CloseSignals(MKName)
+        if (Minsk.MK.List[MKName]) then
+            for _, signal in pairs(Minsk.MK.List[MKName].SignalList) do
+                if (signal:IsValid()) then
+                    local sig = ""
+                    local lenses = signal.Lenses[1]
+
+                    for i = 1, #lenses do
+                        if (lenses[i] == 'R') then
+                            sig = sig.."1"
+                        else
+                            sig = sig.."0"
+                        end
+                    end
+
+                    signal.ControllerLogic = true
+                    signal.Sig = sig
+                    signal.ARSSpeedLimit = 0
+                end
+            end
+        end
+    end
+
+    function Minsk.MK.OpenSignals(MKName)
+        if (Minsk.MK.List[MKName]) then
+            for _, signal in pairs(Minsk.MK.List[MKName].SignalList) do
+                if (signal:IsValid()) then
+                    signal.ControllerLogic = false
+                end
+            end
+        end
+    end
+
+end
+
 function Minsk.MK.CloseButtonLock(MKName)
-    if (Minsk.MK.CloseButtonList[MKName]) then
-        Minsk.MK.CloseButtonList[MKName]:Fire("Lock")
+    if (Minsk.MK.List[MKName]) then
+        if (IsValid(Minsk.MK.List[MKName].CloseButton)) then
+            Minsk.MK.List[MKName].CloseButton:Fire("Lock")
+        end 
     end
 end
 
 function Minsk.MK.CloseButtonUnlock(MKName)
-    if (Minsk.MK.CloseButtonList[MKName]) then
-        Minsk.MK.CloseButtonList[MKName]:Fire("Unlock")
+    if (IsValid(Minsk.MK.List[MKName].CloseButton)) then
+        Minsk.MK.List[MKName].CloseButton:Fire("Unlock")
     end
 end
 
+function Minsk.MK.IndicatorLock(MKName)
+    if (IsValid(Minsk.MK.List[MKName].Indicator)) then
+        Minsk.MK.List[MKName].Indicator:Fire("Skin", "1") 
+    end
+end
+
+function Minsk.MK.IndicatorUnlock(MKName)
+    if (IsValid(Minsk.MK.List[MKName].Indicator)) then
+        Minsk.MK.List[MKName].Indicator:Fire("Skin", "0") 
+    end
+end
+
+
 function Minsk.MK.OpenButtonLock(MKName)
-    if (Minsk.MK.OpenButtonList[MKName]) then
-        Minsk.MK.OpenButtonList[MKName]:Fire("Lock")
+    if (IsValid(Minsk.MK.List[MKName].OpenButton)) then
+        Minsk.MK.List[MKName].OpenButton:Fire("Lock")
     end
 end
 
 function Minsk.MK.OpenButtonUnlock(MKName)
-    if (Minsk.MK.OpenButtonList[MKName]) then
-        Minsk.MK.OpenButtonList[MKName]:Fire("Unlock")
+    if (IsValid(Minsk.MK.List[MKName].OpenButton)) then
+        Minsk.MK.List[MKName].OpenButton:Fire("Unlock")
     end
 end
 
@@ -98,18 +230,6 @@ function Minsk.MK.AllButtonUnlock()
     for _, MKName in pairs(MKNameList) do
         Minsk.MK.CloseButtonUnlock(MKName)
         Minsk.MK.OpenButtonUnlock(MKName)
-    end
-end
-
-function Minsk.MK.IndicatorLock(MKName)
-    if (Minsk.MK.IndicatorList[MKName]) then
-        Minsk.MK.IndicatorList[MKName]:Fire("Skin", "1") 
-    end
-end
-
-function Minsk.MK.IndicatorUnlock(MKName)
-    if (Minsk.MK.IndicatorList[MKName]) then
-        Minsk.MK.IndicatorList[MKName]:Fire("Skin", "0") 
     end
 end
 
@@ -138,4 +258,4 @@ end)
 
 
 -- Initialization
-hook.Add("InitPostEntity", "MinskMKInitialize", Minsk.MK.Initialize)
+hook.Add("InitPostEntity", "Minsk.MK.Initialize", Minsk.MK.Initialize)
