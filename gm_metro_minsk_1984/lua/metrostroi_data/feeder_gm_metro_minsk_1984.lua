7b14b0796c48ce5e8a0db662964e17d178ed4547
robilkot <timur.robilko@gmail.com>
2021-11-17 22:52:40 +0300

    add signaling for singleplayer

---
+++ gm_metro_minsk_1984/lua/metrostroi_data/feeder_gm_metro_minsk_1984.lua
@@ -0,0 +1,50 @@
+{
+    "1" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "2" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+	"3" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "4" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "5" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "6" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+	"7" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "8" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "9" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "10" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+	"11" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    },
+    "12" : {
+        "VoltageLimit" : 750,
+        "CurrentLimit" : 10000
+    }
+}
