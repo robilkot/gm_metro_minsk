34512f63b22426e30eb8e3724d39a394a2ffb286
klusandr <65304648+klusandr@users.noreply.github.com>
2022-07-28 02:32:44 +0300

    fix feeders current and info; add save feeders

--- gm_metro_minsk_1984/lua/metrostroi/sv_minsk_feeder.lua
+++ gm_metro_minsk_1984/lua/metrostroi/sv_minsk_feeder.lua
@@ -10,29 +10,31 @@
 -----------------------------------------------------------------------------------------
 
 
-local function getFile(path, name)
+local function getFile(path)
     local data,found
-    if file.Exists(Format(path..".txt",name),"DATA") then
-        data= util.JSONToTable(file.Read(Format(path..".txt",name),"DATA"))
+    if file.Exists(path..".txt", "DATA") then
+        data = util.JSONToTable(file.Read(path..".txt", "DATA"))
         found = true
     end
-    if not data and file.Exists(Format(path..".lua",name),"LUA") then
-        data = util.JSONToTable(file.Read(Format(path..".lua",name),"LUA"))
+    if not data and file.Exists(path..".lua", "LUA") then
+        data = util.JSONToTable(file.Read(path..".lua", "LUA"))
         found = true
     end
-    if not found then
-        --print(Format("%s definition file not found: %s",id,Format(path,name)))
-        return
-    elseif not data then
-        --print(Format("Parse error in %s %s definition JSON",id,Format(path,name)))
-        return
-    end
+    
     return data
 end
 
-local data = getFile("metrostroi_data/feeder_%s", game.GetMap())
-if (not data) then return end       -- Return, if config file dont found
+local function setFile(path, data)
+    path = path..".txt"
+    if not file.Exists(path, "DATA") then
+        file.Append(path)
+    end
 
+    file.Write(path, data)
+end
+
+local data = getFile("metrostroi_data/feeder_"..game.GetMap())
+if (not data) then return end       -- Return, if config file dont found
 
 ----   API definition   ----
 
@@ -174,14 +176,26 @@ function Metrostroi.Feeder.GetInfoString()
     
     for feeder, _ in pairs(Metrostroi.FeedersOn) do
         info = info..feeder..": "
-        info = info.."\tCurrent limit: "..Metrostroi.CurrentLimits[feeder].."; "
-        info = info.."Voltages: "..Metrostroi.VoltagesLimits[feeder].."; "
-        info = info.."State: "..((Metrostroi.FeedersOn[feeder]) and "On" or "Off")..";\n"
+        info = info.." "..Metrostroi.VoltagesLimits[feeder].."; "
+        info = info..Metrostroi.CurrentLimits[feeder].."; "
+        info = info..((Metrostroi.FeedersOn[feeder]) and "On" or "Off")..";\n"
     end
 
     return info
 end
 
+function Metrostroi.Feeder.Save()
+    local data = {} 
+
+    for feeder, voltageValue in pairs(Metrostroi.VoltagesLimits) do
+        data[feeder] = {
+            ["VoltageLimit"] = voltageValue,
+            ["CurrentLimit"] = Metrostroi.CurrentLimits[feeder]
+        }
+    end
+
+    setFile("metrostroi_data/feeder_"..game.GetMap(), util.TableToJSON(data, true))
+end
 
 ----   Hook inject   ----
 
@@ -213,6 +227,103 @@ timer.Simple(1, function ()
 end)
 
 
+----   New Metrostroi electric consumptionThink hook   ----
+-- Turns off when a new version of the Metrostroi addon is released.
+if (Metrostroi.Version == 1537278077) then
+    timer.Simple(0.5, function()
+        local function consumeFromFeeder(inCurrent, inFeeder)
+            if inFeeder then
+                Metrostroi.Currents[inFeeder] = Metrostroi.Currents[inFeeder] + inCurrent*0.4
+            else
+                Metrostroi.Current = Metrostroi.Current + inCurrent*0.4
+            end
+        end
+        
+        local prevTime
+
+        hook.Remove("Think", "Metrostroi_ElectricConsumptionThink")
+        hook.Add("Think", "Metrostroi_ElectricConsumptionThink", function()
+            -- Change in time
+            prevTime = prevTime or CurTime()
+            local deltaTime = (CurTime() - prevTime)
+            prevTime = CurTime()
+        
+            -- Calculate total rate
+            Metrostroi.TotalRateWatts = 0
+            Metrostroi.Current = 0
+            for k,v in pairs(Metrostroi.Currents) do Metrostroi.Currents[k] = 0 end
+            local bogeys = ents.FindByClass("gmod_train_bogey")
+            for _,bogey in pairs(bogeys) do
+                if bogey.Feeder then
+                    Metrostroi.Currents[bogey.Feeder] = Metrostroi.Currents[bogey.Feeder] + bogey.DropByPeople
+                else
+                    Metrostroi.Current = Metrostroi.Current + bogey.DropByPeople
+                end
+            end
+            for _,class in pairs(Metrostroi.TrainClasses) do
+                local trains = ents.FindByClass(class)
+                for _,train in pairs(trains) do
+                    if train.Electric then
+                        if train.Electric.EnergyChange then Metrostroi.TotalRateWatts = Metrostroi.TotalRateWatts + math.max(0, train.Electric.EnergyChange) end
+                        local current = math.max(0, train.Electric.Itotal or 0) -  math.max(0, train.Electric.Iexit or 0)
+        
+                        local fB = IsValid(train.FrontBogey) and train.FrontBogey
+                        local rB = IsValid(train.RearBogey) and train.RearBogey
+                        if fB and (not fB.ContactStates or (not fB.ContactStates[1] and not fB.ContactStates[2])) then fB = nil end -- Don't have contact with TR
+                        if rB and (not rB.ContactStates or (not rB.ContactStates[1] and not rB.ContactStates[2])) then rB = nil end -- Don't have contact with TR
+        
+                        local fBfeeder = fB and fB.Feeder
+                        local rBfeeder = rB and rB.Feeder
+        
+                        if fBfeeder then
+                            if not rBfeeder or fBfeeder == rBfeeder then
+                                consumeFromFeeder(current, fBfeeder) -- Feeders are same
+                            else
+                                consumeFromFeeder(current * 0.5, fBfeeder) -- Feeders are different
+                            end
+                        end
+        
+                        if rBfeeder then
+                            if not fBfeeder then
+                                consumeFromFeeder(current, rBfeeder) -- Feeders are same
+                            elseif fBfeeder ~= rBfeeder  then
+                                consumeFromFeeder(current * 0.5, rBfeeder) -- Feeders are different
+                            end
+                        end
+                        if not rBfeeder and not fBfeeder then consumeFromFeeder(current) end
+                    end
+                end
+            end
+            -- Ignore invalid values
+            if Metrostroi.TotalRateWatts > 1e8 then Metrostroi.TotalRateWatts = 0 end
+            if Metrostroi.TotalRateWatts > 0 then
+                -- Calculate total kWh
+                Metrostroi.TotalkWh = Metrostroi.TotalkWh + (Metrostroi.TotalRateWatts/(3.6e6))*deltaTime
+            end
+            -- Calculate total resistance of people on rails and current flowing through
+            --local Rperson = 0.613
+            --local Iperson = Metrostroi.Voltage / (Rperson/(Metrostroi.PeopleOnRails + 1e-9))
+            --Metrostroi.Current = Metrostroi.Current + Iperson
+        
+            -- Check if exceeded global maximum current
+            if Metrostroi.Current > GetConVar("metrostroi_current_limit"):GetInt() then
+                Metrostroi.VoltageRestoreTimer = CurTime() + 7.0
+                print(Format("[!] Power feed protection tripped: current peaked at %.1f A",Metrostroi.Current))
+            end
+        
+            local voltage = math.max(0,GetConVar("metrostroi_voltage"):GetInt())
+        
+            -- Calculate new voltage
+            local Rfeed = 0.03 --25
+            Metrostroi.Voltage = voltage - Metrostroi.Current*Rfeed
+            if CurTime() < Metrostroi.VoltageRestoreTimer then Metrostroi.Voltage = 0 end
+            for i in pairs(Metrostroi.Voltages) do
+                Metrostroi.Voltages[i] = math.max(0,voltage - Metrostroi.Currents[i]*Rfeed)
+            end
+            --print(Format("%5.1f v %.0f A",Metrostroi.Voltage,Metrostroi.Current))
+        end) 
+    end)
+end   
 ----   Console command definition   ----
 
 concommand.Add("metrostroi_current_limit_feeder", function(ply, _, args)
@@ -253,8 +364,13 @@ concommand.Add("metrostroi_feeder_off", function(ply, _, args)
     end
 end)
 
-concommand.Add("metrostroi_feeder_info", function (ply, _, _)
+concommand.Add("metrostroi_feeder_info", function(ply, _, _)
     ply:ChatPrint(Metrostroi.Feeder.GetInfoString())
+end)
+
+concommand.Add("metrostroi_feeder_save", function(ply, _, _)
+    if (ply and ply != NULL and not ply:IsAdmin()) then return end
+    Metrostroi.Feeder.Save()
 end)
 
 -- Initialization
